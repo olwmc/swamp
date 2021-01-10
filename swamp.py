1a41bcddcc4a72cf20ce19ea9415bb4ac5ad68d7
olwmc <olwmc@protonmail.ch>
2021-01-09 20:50:02 -0500

    Added: Simple debug mode

--- swamp.py
+++ swamp.py
@@ -5,18 +5,28 @@ from parser import *
 stack = []
 env = make_env()
 
-# Check if execute
-if len(sys.argv) > 1 and sys.argv[1] == "-e":
+
+# Check if evaluate
+if "-e" in sys.argv:
     try:
-        run_program(sys.argv[2], stack, env)
+        run_program(sys.argv[-1], stack, env)
 
     except:
         print("Invalid program")
 
-elif len(sys.argv) > 1:
-    run_program_from_file(sys.argv[1], stack, env)
-
-else:
+# Else REPL
+elif len(sys.argv) == 1:
     while(True):
         inp = input("[🐊]> ")
         run_program(inp, stack, env)
+
+# Else run from file
+else:
+    run_program_from_file(sys.argv[-1], stack, env)
+
+
+# """Debug""" mode
+if "-d" in sys.argv:
+    print("STACK: ", end="")
+    print(stack)
+    print(env)
